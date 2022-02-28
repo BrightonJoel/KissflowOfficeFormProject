cf59ca4c2d1d3d9126408cb72454a189eeef966e
BrightonJoel <sbjoel112000@gmail.com>
2022-03-01 00:45:56 +0530

    created enviroment for the mongo database

--- office-form-backend/server.py
+++ office-form-backend/server.py
@@ -1,6 +1,26 @@
 from flask import Flask
+import pymongo
+
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost",
+        port="27017",
+        serverSelectionTimeoutMS = 1000
+    )
+    mongo.server_info()
+
+    database = mongo.company
+except Exception as ex :
+    print(ex)
 
 app = Flask(__name__)
+
+@app.route("/users")
+def createUser():
+    try:
+        
+    except Exception as ex:
+        print(ex)
 
 @app.route("/members")
 def members():
