cd1b8718810b4d8d2709613fb5fd0598a3dffcc1
BrightonJoel <sbjoel112000@gmail.com>
2022-03-07 11:55:16 +0530

    form submition creatd

--- office-form-backend/server.py
+++ office-form-backend/server.py
@@ -1,8 +1,13 @@
 from flask import Flask, Response, request
 import pymongo
 import json 
+import timestring
+import dateutil.parser
+from flask_cors import CORS
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 try:
 
     mongo = pymongo.MongoClient(
@@ -17,22 +22,36 @@ except Exception as ex :
 
 
 
-@app.route("/users", methods=['POST'])
-def createUser():
+@app.route("/api/leaveFormSubmit", methods=['POST'])
+def createLeaveForm():
     try: 
-        user= {
-        "name": request.form["name"], 
-        "empid": request.form["empid"], 
-        "designation":"Intern",
-        "email" : "sbjoel112000@gmail.com",
-        "firstPointOfContact": "brightons@karunya.edu.in",
-        "secondPointOfContact": "sbytube112000@gmail.com"
+        print(request.json["empid"],request.json["email"],
+        request.json["fromdate"],request.json["todate"])
+        form= {
+        "empid": int(request.json["empid"]), 
+        "email": request.json["email"], 
+        "fromdate": dateutil.parser.isoparse(request.json["fromdate"]),
+        "todate": dateutil.parser.isoparse(request.json["todate"]),
+        "reason" : request.json["reason"]
         }
-        dbResponse = database.users.insert_one(user)
-        print(dbResponse.inserted_id)
+        dbResponse = database.forms.insert_one(form)
+       
+
+        user = database.users.find_one({"empid": int(request.json["empid"])})
+        user = json.load(user)
+        s = Serializer('WEBSITE_SECRET_KEY', 60*30) # 60 secs by 30 mins
+        token = s.dumps(
+            {'user_name':user['name'] ,
+            'user_designation': user['designation'],
+            'user_empid': user['empid'],
+            'user_email': user['email'],
+            'form_to_date': form['todate'],
+            'form_reason' : form['reason'],
+            'form_from_date':form['fromdate']}).decode('utf-8')
+        print(url_for('get_file', token=token))
         return Response(
             response= json.dumps(
-                {"message":"User created!", 
+                {"message":"Form submitted and the email is sent", 
                 "id": f"{dbResponse.inserted_id}"
                 }),
             status= 200,
@@ -42,7 +61,14 @@ def createUser():
 
     except Exception as ex:
         print(ex)
-    return "False"
+        return Response(
+            response= json.dumps(
+                {"message": str(ex)
+                }),
+            status= 400,
+            mimetype="application/json"
+
+        )
 
 @app.route("/members")
 def members():
--- office-form-frontend/src/forms/leaveForm.js
+++ office-form-frontend/src/forms/leaveForm.js
@@ -2,25 +2,75 @@ import React, { useState } from "react";
 import { LeaveFormMain } from "./leaveFormStyles";
 
 export default function LeaveFrom() {
-    const [statusMessage, setStateMessage] = useState(null);
+    const [statusMessage, setStatusMessage] = useState(null);
+    const [empid, setEmpid] = useState("");
+    const [empEmail, setEmpEmail] = useState("");
+    const [fromDate, setFromDate] = useState("");
+    const [toDate, setToDate] = useState("")
+    const [reason, setReason] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          let res = await fetch("http://localhost:5000/api/leaveFormSubmit", {
+            method: "POST",
+            headers : {
+                'Content-Type':'application/json'
+          },
+            body: JSON.stringify({
+              empid: empid,
+              email: empEmail,
+              fromdate: fromDate,
+              todate: toDate,
+              reason: reason
+            }),
+          });
+          let resJson = await res.json();
+          if (res.status === 200) {
+            setEmpid("");
+            setEmpEmail("");
+            setFromDate("");
+            setToDate("");
+            setReason("")
+            setStatusMessage(resJson['message']);
+          } else {
+            setStatusMessage(resJson['message']);
+          }
+          
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+
     return (
         <LeaveFormMain>
             <h1>Leave Request form</h1>
             <form>
                 <label htmlFor="EmpId">Employee ID* : </label>
-                <input htmlFor="EmpId" type="text" autoFocus required />
+                <input htmlFor="EmpId" type="text" autoFocus required 
+                value={empid}  onChange={(event) => setEmpid(event.target.value)} />
+
                 <label htmlFor="EmpEmail" >Company Email ID* : </label>
-                <input htmlFor="EmpEmail" type="email" required />
+                <input htmlFor="EmpEmail" type="email" required 
+                value={empEmail} onChange={(event) => setEmpEmail(event.target.value)}/>
+
                 <label htmlFor="fromdate">From date* : </label>
-                <input htmlFor="fromdate" type="date" required></input>
+                <input htmlFor="fromdate" type="date" required
+                value={fromDate} onChange={(event) => setFromDate(event.target.value)}/>
+
                 <label htmlFor="todate">To date* : </label>
-                <input htmlFor="todate" type="date" required />
+                <input htmlFor="todate" type="date" required 
+                value={toDate} onChange={(event) => setToDate(event.target.value)}/>
+
                 <label htmlFor="reason">Reason for leave* : </label>
-                <textarea rows="5" htmlFor="reason" minLength="10" required></textarea>
+                <textarea rows="5" htmlFor="reason" minLength="10" required
+                value={reason}  onChange={(event) => setReason(event.target.value)}></textarea>
+
                 {statusMessage ? (
                     <p>{statusMessage}</p>
                 ) : (console.log("Message is null"))}
-                <button type="submit">Submit</button>
+                <button type="submit" onClick = {handleSubmit}>Submit</button>
                 <button type="reset">Reset</button>
             </form>
 
