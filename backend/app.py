cc0cae9d637fe7e77e893fe1a0f07b1411cc782f
Gladson-Samuel-S <samuelgladson4@gmail.com>
2022-06-28 15:40:52 +0530

    Initialize React Project :fireworks:

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,23 @@
-from curses import flash
 from imp import reload
 from flask import Flask
-import importlib 
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p> <h1> Brighton </h1>"
+    return "<p>Hello, World!</p> <h1 style='color: red;'> Brighton </h1>"
+
+
+@app.route("/products")
+def products():
+    products = ["Socks", "Shoes", "Shirts"]
+    return json.dumps(products)
+
+@app.route('/product/<int:num>', methods = ['GET'])
+def product(num):
+    products = ["Socks", "Shoes", "Shirts"]
+    return json.dumps(products[num])
+
 
 app.run(debug=True, use_reloader= True)
